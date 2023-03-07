3976e41a3eb4a508651e230b5933593ec631df31
Asko <asko.hulkko@voco.ee>
2023-03-07 19:38:32 +0200

    main game

--- question_chose_panel.py
+++ question_chose_panel.py
@@ -1,8 +1,7 @@
 import tkinter as tk
-from tkinter import ttk
 
 
-class tkinterApp(tk.Tk):
+class Vocovillak(tk.Tk):
 
     # __init__ function for class tkinterApp
     def __init__(self, *args, **kwargs):
@@ -21,7 +20,7 @@ class tkinterApp(tk.Tk):
 
         # iterating through a tuple consisting
         # of the different page layouts
-        for F in (StartPage, Dice, Game, Question, Pointsum, Pointtotal):
+        for F in (StartPage, Game1):
             frame = F(container, self)
 
             # initializing frame of that object from
@@ -38,45 +37,81 @@ class tkinterApp(tk.Tk):
     def show_frame(self, cont):
         frame = self.frames[cont]
         frame.tkraise()
-class Game(tk.Frame):
+
+#The start page.
+
+class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
+        #Makes a background.
+        C = tk.Canvas(self, bg="dark blue", height=1080, width=1920)
 
-top = tk.Tk()
-top.geometry("1920x1080")
+        #Creates a button to continue to the next page.
+        B = tk.Button(C, text="START", fg="gold", bg="#2a3698", font=("Impact", 45), command=lambda: controller.show_frame(Game1))
 
-#The main shape of the question board.
-C = tk.Canvas(top, bg="#2a3698", height=1080, width=1920)
+        #Moves the button into the correct place.
+        B.pack()
+        B.place(bordermode="outside", height=200, width=500, x=700, y=700)
 
-#Creates a rectangle for the board.
-square = C.create_rectangle(100, 980, 1820, 100, width=10, outline="gold")
+        #Makes the title of the game.
+        L = tk.Label(C, text="VOCO-VILLAK", fg="gold", bg="#2a3698", font=("Impact", 80))
 
-#Creates horizontal lines for the board.
-hz_line1 = C.create_line(100, 240, 1820, 240, width=10, fill="gold")
-hz_line2 = C.create_line(100, 390, 1820, 390, width=10, fill="gold")
-hz_line3 = C.create_line(100, 540, 1820, 540, width=10, fill="gold")
-hz_line4 = C.create_line(100, 690, 1820, 690, width=10, fill="gold")
-hz_line5 = C.create_line(100, 840, 1820, 840, width=10, fill="gold")
+        #Moves the title to the correct place.
+        L.pack(pady=0, padx=0)
+        L.place(bordermode="outside", height=200, width=700, x=600, y=300)
 
-#Creates vertical line for the board.
-vr_line1 = C.create_line(540, 100, 540, 980, width=10, fill="gold")
-vr_line2 = C.create_line(955, 100, 955, 980, width=10, fill="gold")
-vr_line3 = C.create_line(1370, 100, 1370, 980, width=10, fill="gold")
+        C.pack(fill="both", expand=True)
 
-#The buttons to get to a question.
 
-B1 = tk.Button(C, text=100, bg="#2a3698", fg="white", activebackground="#3d4dd2", font=("Impact", 45), command=lambda: controller.show_frame(Question))
+class Job(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
 
-B1.pack(pady=0, padx=0)
-B1.place(bordermode="outside", height=140, width=430, x=105, y=245)
+        #
+        C = tk.Canvas(self, bg="#2a3698", height=1080, width=1920)
 
-# The labels for categories.
+        B1 = tk.Button(C, text=, bg="#2a3698", fg="white", activebackground="#3d4dd2", font=("Impact", 45),command=lambda: controller.show_frame(Question))
 
-L = tk.Label(C, text="ITa", bg="#2a3698", fg="white", font=("Impact", 50))
+class Game1(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
 
-L.pack()
-L.place(bordermode="outside", height=130, width=430, x=105, y=105)
+        #The main shape of the question board.
+        C = tk.Canvas(self, bg="#2a3698", height=1080, width=1920)
 
-C.pack(fill="both", expand=True)
-top.mainloop()
+        #Creates a rectangle for the board.
+        rectangle = C.create_rectangle(100, 980, 1820, 100, width=10, outline="gold")
+
+        #Creates horizontal lines for the board.
+        hz_line1 = C.create_line(100, 240, 1820, 240, width=10, fill="gold")
+        hz_line2 = C.create_line(100, 390, 1820, 390, width=10, fill="gold")
+        hz_line3 = C.create_line(100, 540, 1820, 540, width=10, fill="gold")
+        hz_line4 = C.create_line(100, 690, 1820, 690, width=10, fill="gold")
+        hz_line5 = C.create_line(100, 840, 1820, 840, width=10, fill="gold")
+
+        #Creates vertical line for the board.
+        vr_line1 = C.create_line(540, 100, 540, 980, width=10, fill="gold")
+        vr_line2 = C.create_line(955, 100, 955, 980, width=10, fill="gold")
+        vr_line3 = C.create_line(1370, 100, 1370, 980, width=10, fill="gold")
+
+        #The buttons to get to a question.
+
+        B1 = tk.Button(C, text=100, bg="#2a3698", fg="white", activebackground="#3d4dd2", font=("Impact", 45), command=lambda: controller.show_frame(Question))
+
+        B1.pack(pady=0, padx=0)
+        B1.place(bordermode="outside", height=140, width=430, x=105, y=245)
+
+        # The labels for categories.
+
+        L = tk.Label(C, text="ITa", bg="#2a3698", fg="white", font=("Impact", 50))
+
+        L.pack()
+        L.place(bordermode="outside", height=130, width=430, x=105, y=105)
+
+        C.pack(fill="both", expand=True)
+
+
+
+app = Vocovillak()
+app.mainloop()
