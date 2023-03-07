994a4ee98d61eac7827d609efc4479e11e326dfb
Rasmus <rasmus.klaaser@voco.ee>
2023-03-08 00:39:17 +0200

    Possible answers

---
+++ QUESTIONS VALUED.py
@@ -0,0 +1,12 @@
+#JAGAMINE KAHTE ERINEVA LISIT
+#Questions below are valued with 2 possible answers
+#LEGENDS: hob-HOBBIES, ope-Õppetöö, voco-Why voco
+question_hob_1 = ["Kas VOCO-s on tasuta jõusaal?   (JAH/EI)", "JAH", 100]
+questions_hob_4 = ["Koolis on Improring.   (ÕIGE/VALE)", "ÕIGE", 400]
+questions_hob_5 = ["Väiketehnika ringi on tehnikahuvilised, kelle sooviks on ehitada võistlusmasinad ja lisaks ka nendega kestvussõidu võistlustel osaleda.   (ÕIGE/VALE)", "ÕIGE", 500]
+questions_ope_3 = ["Kas VOCO-s on õpiränded tasuta?   (JAH/EI)", "JAH", 300]
+questions_ope_4 = ["Kas praktikakohast võib kujuneda esimene ja kindel töökoht.   (KINDLASTI/PIGEM MITTE)", "KINDLASTI", 400]
+questions_ope_5 = ["VOCO-s on tunniplaan üksluine.  (ÕIGE/VALE SEE MUUTUB IGA NÄDAL)", "VALE SEE MUUTUB IGA NÄDAL", 500]
+questions_voco_1 = ["Kas Tartu Rakenduslikus Kolledžis omandad koos tööalaste praktiliste oskustega nii kutse kui keskhariduse.  (JAH/EI)", "JAH", 100]
+questions_voco_3 = ["Kas Tartu Rakenduslik Kolledž toetab õpilaste osalemist kutsevõistlustel.  (KINDLASTI MITTE/JAH)", "JAH", 300]
+questions_voco_4 = ["Mitu osakonda on VOCO-s?  (7/11)", "7", 400]
