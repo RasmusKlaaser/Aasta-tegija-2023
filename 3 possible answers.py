f94294c10221ab18fd4f436f279e7420fd4da8f1
Rasmus <rasmus.klaaser@voco.ee>
2023-03-08 00:37:48 +0200

    3 POSSIBLE ANSWERS

---
+++ 3 possible answers.py
@@ -0,0 +1,7 @@
+#JAGAMINE KAHTE ERINEVA LISIT
+#Questions below are valued with 3 possible answers
+#LEGENDS: hob-HOBBIES, ope-Õppetöö, voco-Why voco
+questions_hob_2 = ["Mis on VOCO rahvatantsurühma nimi?   (TRISKEL/MÕMMIKUD/TORNUM)", "TRISKEL", 200]
+questions_hob_3 = ["Mis pille saab kooli bändis mängida?   (ELEKTRIKITARRI/OREL/OBOE)", "ELEKTRIKITARRI", 300]
+questions_voco_2 = ["Kus toimub õppetöö ja praktika? (POMMIAUGUS/LABORITES/PÕLLUL)", "LABORITES", 200]
+questions_voco_5 = ["Mis on Voco oma enda e-päeviku nimi?  (E-KOOL/STUUDIUM/SISEVEEB)", "SISEVEEB", 500]
