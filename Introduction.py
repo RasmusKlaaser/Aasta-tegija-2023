b68828c61de73147de40ce0b0df9f93affd008de
Rasmus <rasmus.klaaser@voco.ee>
2023-03-07 10:26:31 +0200

    Introduction

---
+++ Introduction.py
@@ -0,0 +1,7 @@
+valik = input("Palun vali eriala: tarkvaraarendaja või süsteemide spetsialist: ")
+if valik.lower() == "tarkvaraarendaja":
+    print("Olete valinud tarkvaraarendaja")
+if valik.lower() == "süsteemide spetsialist":
+    print("Olete valinud süsteemide spetsialisti")
+else:
+    print("Vale")
