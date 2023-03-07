953e1ca95f3419b0285e5ecdcd5791d6942e3e5c
Rasmus <rasmus.klaaser@voco.ee>
2023-03-07 12:32:54 +0200

    Main code

--- Introduction.py
+++ Introduction.py
@@ -1,7 +1,44 @@
-valik = input("Palun vali eriala: tarkvaraarendaja või süsteemide spetsialist: ")
+#Introduction
+valik = input("Palun vali eriala: (tarkvaraarendaja) või (süsteemide spetsialist): ")
 if valik.lower() == "tarkvaraarendaja":
     print("Olete valinud tarkvaraarendaja")
-if valik.lower() == "süsteemide spetsialist":
+elif valik.lower() == "süsteemide spetsialist":
     print("Olete valinud süsteemide spetsialisti")
 else:
-    print("Vale")
+    print("Vale sisestus")
+
+# Rolling a dice
+import random
+roll_dice = input("Write (START) to dice roll: ")
+
+if roll_dice == "start".upper():
+   possible_results = range(1, 5)
+   result = random.choice(possible_results)
+   print("Result of dice rolling is: " + str(result))
+else:
+    print("Wrong input")
+
+if valik.lower() == "tarkvaraarendaja":
+    question_1 = "QUESTION"
+    question_2 = "Question"
+    question_3 = "question"
+    question_4 = "question"
+    question_5 = "question"
+elif valik.lower() == "süsteemide spetsialist":
+    question_1 = "dad"
+    question_2 = "dad"
+    question_3 = "dad"
+    question_4 = "dad"
+    question_5 = "dad"
+
+if result == 1:
+    print(question_1)
+elif result == 2:
+    print(question_2)
+elif result == 3:
+    print(question_3)
+elif result == 4:
+    print(question_4)
+elif result == 5:
+    print(quetion_5)
+
