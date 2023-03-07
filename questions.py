0bd8977645155b1d9fd7a7af8ce981a02a37be59
Rasmus <rasmus.klaaser@voco.ee>
2023-03-07 23:18:57 +0200

    ALL THE OTHERS 15 QUESTIONS (COMPLETE)

---
+++ questions.py
@@ -0,0 +1,19 @@
+vastuseid_kokku = 15
+#All questions below are meant for hobbys
+question_hob_1 = "Kas VOCO-s on tasuta jõusaal?   (JAH/EI)"
+questions_hob_2 = "Mis on VOCO rahvatantsurühma nimi?   (TRISKEL/MÕMMIKUD/TORNUM)"
+questions_hob_3 = "Mis pille saab kooli bändis mängida?   (ELEKTRIKITARRI/OREL/OBOE)"
+questions_hob_4 = "Koolis on Improring.   (ÕIGE/VALE)"
+questions_hob_5 = "Väiketehnika ringi on tehnikahuvilised, kelle sooviks on ehitada võistlusmasinad ja lisaks ka nendega kestvussõidu võistlustel osaleda.   (ÕIGE/VALE)"
+
+questions_ope_1 = "Millised erinevad õpped ja koolitused on VOCO-s? (USUÕPE/KUTSEÕPE/FILOSOOFIA)"
+questions_ope_2 = "Millised toetused on VOCO's?   (ÕPPETOETUS/EMOTSIONAALNE/SÕIDUKULUDE HÜVITAMINE)"
+questions_ope_3 = "Kas VOCO-s on õpiränded tasuta?   (JAH/EI)"
+questions_ope_4 = "Kas praktikakohast võib kujuneda esimene ja kindel töökoht.   (KINDLASTI/PIGEM MITTE)"
+questions_ope_5 = "VOCO-s on tunniplaan üksluine.  (ÕIGE/VALE SEE MUUTUB IGA NÄDAL)"
+
+questions_voco_1 = "Kas Tartu Rakenduslikus Kolledžis omandad koos tööalaste praktiliste oskustega nii kutse kui keskhariduse.  (JAH/EI)"
+questions_voco_2 = "Kus toimub õppetöö ja praktika? (POMMIAUGUS/LABORITES/PÕLLUL)"
+questions_voco_3 = "Kas Tartu Rakenduslik Kolledž toetab õpilaste osalemist kutsevõistlustel.  (KINDLASTI MITTE/JAH)"
+questions_voco_4 = "Mitu osakonda on VOCO-s?  (7/11)"
+questions_voco_5 = "Mis on Voco oma enda e-päeviku nimi?  (E-KOOL/STUUDIUM/SISEVEEB)"
