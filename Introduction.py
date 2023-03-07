8534ed59a184a34f2ed5ced4cd4cb21f275a9b36
Rasmus <rasmus.klaaser@voco.ee>
2023-03-07 22:10:00 +0200

    Introducttion with scoring system

--- Introduction.py
+++ Introduction.py
@@ -18,33 +18,135 @@ result = 0
 if otsus == 1:
     roll_dice = input("Write (START) to dice roll: ")
 
-    if roll_dice == "start".upper():
+    if roll_dice == "START":
        possible_results = [1, 2, 3, 4, 5]
        result = random.choice(possible_results)
        print("Result of dice rolling is: " + str(result))
     else:
         print("Wrong input")
+#Making sure that u can write both ways
 
-    if valik.lower() == "tarkvaraarendaja":
-        question_1 = "QUESTION"
-        question_2 = "Question"
-        question_3 = "question"
-        question_4 = "question"
-        question_5 = "question"
-    elif valik.lower() == "süsteemide spetsialist":
-        question_1 = "dad"
-        question_2 = "dad"
-        question_3 = "dad"
-        question_4 = "dad"
-        question_5 = "dad"
+vastuseid_kokku = 20
+#Punktisumma arvutamine
+#all questions below are meant for ITA!!!!
+question_1 = "Kas selle erialaga saab olulise eelise õpigute jätkamiseks kõrgkoolis ja võib spetsialiseeruda IT valdkonna profiks?    (JAH/EI)"
+question_2 = "Milline eriala on eriliselt kiiresti kasvav ja maailma tulevik?     (TARKVARAARENDJA/VEEBIHALDUR)"
+question_3 = "Tegu on globaalse kasvava ettevõtlusvaldkonnaga, kus heade spetsialistide järele on pidevalt suur nõudlus, mistõttu tarkvaraarendaja leiab hõlpsasti tööd ka väljaspool Eestit.      (ÕIGE/VALE)"
+question_4 = "Tarkvaraarendajad saavad töötada vähestes valdkondades.    (ÕIGE/VALE)"
+question_5 = "Eesti IT-sektoris on palgatase keskmisest kõrgem ning tarkvaraarendaja töötasu alumine määr võib ületada Eesti keskmise töötasu. (ÕIGE/VALE)"
 
-    if result == 1:
-        print(question_1)
-    elif result == 2:
-        print(question_2)
-    elif result == 3:
-        print(question_3)
-    elif result == 4:
-        print(question_4)
-    elif result == 5:
-        print(question_5)
+row1 = 100
+row2 = 200
+row3 = 300
+row4 = 400
+row5 = 500
+score = 0
+print(question_1)
+ans = input("Vastus: ")
+if ans.lower() == "jah":
+    score += row1
+    vastuseid_kokku -= 1
+else:
+    score -= row1 / 2
+    vastuseid_kokku -= 1
+print(f"Punkti summa on nüüd: {score}")
+print(f"{vastuseid_kokku} vastust on alles.")
+print(question_2)
+ans = input("Vastus: ")
+if ans.lower() == "tarkvaraarendaja":
+    score += row2
+    vastuseid_kokku -= 1
+else:
+    score -= row2 / 2
+    vastuseid_kokku -= 1
+print(f"Punkti summa on nüüd: {score}")
+print(f"{vastuseid_kokku} vastust on nüüd alles")
+print(question_3)
+ans = input("Vastus: ")
+if ans.lower() == "õige":
+    score += row3
+    vastuseid_kokku -= 1
+else:
+    score -= row3 / 2
+    vastuseid_kokku -= 1
+print(f"Punkti summa on nüüd: {score}")
+print(f"{vastuseid_kokku} vastust on nüüd alles")
+print(question_4)
+ans = input("Vastus: ")
+if ans.lower() == "vale":
+    score += row4
+    vastuseid_kokku -= 1
+else:
+    score -= row4 / 2
+    vastuseid_kokku -= 1
+print(f"Punkti summa on nüüd: {score}")
+print(f"{vastuseid_kokku} vastust on nüüd alles")
+print(question_5)
+ans = input("Vastus: ")
+if ans.lower() == "õige":
+    score += row5
+    vastuseid_kokku -= 1
+else:
+    score -= row5 / 2
+    vastuseid_kokku -= 1
+print(f"Punkti summa on nüüd: {score}")
+print(f"{vastuseid_kokku} vastust on nüüd alles")
+
+#ALL THE QUESTIONS BELOW ARE FOR ITS!!!
+question_1_2 = "Kas IT-süsteemide spetsialist saab tööle IT-süsteemide spetsialistina või klienditoespetsialistina?    (SPETSIALIST/KLIENDITUGI)"
+question_2_2 = "Kas tegu on ettevõttevaldkonnaga, kus spetsialistie järele on väike nõudlus?    (JAH/EI)"
+question_3_2 = "Kas IT-spetsialist haldab arvutivõrke, telefoniteenuseid ja vajadusel ehitab müüri?      (JAH/EI)"
+question_4_2 = "Mida tähendab lühend IT?   (FILM/INFO-TEADUS/INFO-TEHNOLOOGIA)"
+question_5_2 = "Milliseid võtmeoskusi on vaja edukaks IT-spetsialisti karjääriks?  (ERINEVATE ARVUTIKEELTE TUNDMINE/INVESTEERIMINE/LOOGILINE MÕTLEMINE)"
+
+print(question_1_2)
+ans = input("Vastus: ")
+if ans.lower() == "spetsialist":
+    score += row1
+    vastuseid_kokku -= 1
+else:
+    score -= row1 / 2
+    vastuseid_kokku -= 1
+print(f"Punkti summa on nüüd: {score}")
+print(f"{vastuseid_kokku} vastust on nüüd alles")
+print(question_2_2)
+ans = input("Vastus: ")
+if ans.lower() == "ei":
+    score += row2
+    vastuseid_kokku -= 1
+else:
+    score -= row2 / 2
+    vastuseid_kokku -= 1
+print(f"Punkti summa on nüüd: {score}")
+print(f"{vastuseid_kokku} vastust on nüüd alles")
+print(question_3_2)
+ans = input("Vastus: ")
+if ans.lower() == "ei":
+    score += row3
+    vastuseid_kokku -= 1
+else:
+    score -= row3 / 2
+    vastuseid_kokku -= 1
+print(f"Punkti summa on nüüd: {score}")
+print(f"{vastuseid_kokku} vastust on nüüd alles")
+print(question_4_2)
+ans = input("Vastus: ")
+if ans.lower() == "info-tehnoloogia":
+    score += row4
+    vastuseid_kokku -= 1
+else:
+    score -= row4 / 2
+    vastuseid_kokku -= 1
+print(f"Punkti summa on nüüd: {score}")
+print(f"{vastuseid_kokku} vastust on nüüd alles")
+print(question_5_2)
+ans = input("Vastus: ")
+if ans.lower() == "loogiline mõtlemine":
+    score += row5
+    vastuseid_kokku -= 1
+else:
+    score -= row5 / 2
+    vastuseid_kokku -= 1
+print(f"Punkti summa on nüüd: {score}")
+print(f"{vastuseid_kokku} vastust on nüüd alles")
+
