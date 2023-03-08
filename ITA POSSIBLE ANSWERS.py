4a764a4d562f0a16c4a30480acd379396c7e088a
Rasmus <rasmus.klaaser@voco.ee>
2023-03-08 09:31:30 +0200

    Erialased küsimused

---
+++ ITA POSSIBLE ANSWERS.py
@@ -0,0 +1,6 @@
+#all questions below are meant for ITA!!!!
+question_1 = ["Kas selle erialaga saab olulise eelise õpigute jätkamiseks kõrgkoolis ja võib spetsialiseeruda IT valdkonna profiks?    (JAH/EI)", "JAH", 100]
+question_2 = ["Milline eriala on eriliselt kiiresti kasvav ja maailma tulevik?     (TARKVARAARENDJA/VEEBIHALDUR)", "TARKVARAARENDAJA", 200]
+question_3 = ["Tegu on globaalse kasvava ettevõtlusvaldkonnaga, kus heade spetsialistide järele on pidevalt suur nõudlus, mistõttu tarkvaraarendaja leiab hõlpsasti tööd ka väljaspool Eestit.      (ÕIGE/VALE)", "õige", 300]
+question_4 = ["Tarkvaraarendajad saavad töötada vähestes valdkondades.    (ÕIGE/VALE)", "VALE", 400]
+question_5 = ["Eesti IT-sektoris on palgatase keskmisest kõrgem ning tarkvaraarendaja töötasu alumine määr võib ületada Eesti keskmise töötasu. (ÕIGE/VALE)", "õige", 500]
---
+++ ITS POSSIBLE ANSWERS.py
@@ -0,0 +1,6 @@
+#ALL THE QUESTIONS BELOW ARE FOR ITS!!!
+question_1_2 = ["Kas IT-süsteemide spetsialist saab tööle IT-süsteemide spetsialistina või klienditoespetsialistina?    (SPETSIALIST/KLIENDITUGI)", "SPETSIALIST", 100]
+question_2_2 = ["Kas tegu on ettevõttevaldkonnaga, kus spetsialistie järele on väike nõudlus?    (JAH/EI)", "EI", 200]
+question_3_2 = ["Kas IT-spetsialist haldab arvutivõrke, telefoniteenuseid ja vajadusel ehitab müüri?      (JAH/EI)", "EI", 300]
+question_4_2 = ["Mida tähendab lühend IT?   (FILM/INFO-TEADUS/INFO-TEHNOLOOGIA)", "INFO-TEHNOLOOGIA", 400]
+question_5_2 = ["Milliseid võtmeoskusi on vaja edukaks IT-spetsialisti karjääriks?  (ERINEVATE ARVUTIKEELTE TUNDMINE/INVESTEERIMINE/LOOGILINE MÕTLEMINE)", "LOOGILINE MÕTLEMINE", 500]
